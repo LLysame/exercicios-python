c5e2144b24947156b8ba5ee2fa57a800a6268e2a
LLysame <athos.48yt@gamil.com>
2022-07-28 08:51:51 -0400

    detalhe

---
+++ como ver uma data.py
@@ -0,0 +1,7 @@
+
+# na mesma linha colocamos esse dois codigos juntos
+# "from datetime import date"
+
+#logo após e usado o "str()" para converter o "date.today()" para string
+#e montando o codigo junto do comando "print" e o comando "str()"
+#print('A data de hoje é ' + str(date.today()))

7fd46b6351bedecc6de1e5ba2091cd527d108660
LLysame <athos.48yt@gamil.com>
2022-07-28 03:53:33 -0400

    exercicio

---
+++ ex002.py
@@ -0,0 +1,6 @@
+# faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vinda.
+
+# resolução
+
+# nome = input('Qual e o seu nome ?')
+# print('E um prazer conhecer você, {}'.format(nome))

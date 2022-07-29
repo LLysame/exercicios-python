ba37f77176747dc8fd1547a4b5454c5fc55585d9
LLysame <athos.48yt@gamil.com>
2022-07-29 09:16:14 -0400

    exercicio

---
+++ ex004.py
@@ -0,0 +1,10 @@
+# faça um programa que leia algo no teclado e mostre os tipos primitivos dele
+
+# t = input('digite algo: ')
+# print('é um numero?: ', t.isnumeric())
+# print('tem espaços?: ', t.isspace())
+# print('e alfabetico?: ', t.isalpha())
+# print('é alfanumerico?: ', t.isalnum())
+# print('ta em maiusculo?: ', t.isupper())
+# print('ta em minusculo?: ', t.islower())
+# print('ele esta captalizado?: ', t.istitle())

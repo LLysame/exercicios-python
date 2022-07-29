b0393e177fe8a1fb7c2b234cb6718aa48b2fe10a
LLysame <athos.48yt@gamil.com>
2022-07-29 08:56:06 -0400

    exercicio

--- ex003.py
+++ ex003.py
@@ -1,5 +1,6 @@
+#faça um programa que some 1 numero com outro numero
+
 # n1 = int(input('digite um numero: '))
 # n2 = int(input('digite outro numero: '))
 # soma = n1*n2
-
 # print('o resultado de {} vezes {} é igual a {}'.format(n1,n2,soma))

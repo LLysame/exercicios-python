6bcdef6ee1bb0da32845c4056f5555639bb3be35
LLysame <athos.48yt@gamil.com>
2022-07-30 12:29:16 -0400

    exercicio

---
+++ ex008.py
@@ -0,0 +1,6 @@
+# Escreva um programa que leia um valor em metros e o exiba 
+# convertido em centimetros e milimetros
+
+# m = int(input('coloque o valor em metros: '))
+# print('O centimetro é {} ' .format(m*100))
+# print('O milimetro é {} ' .format(m*1000))
---
+++ ex009.py
@@ -0,0 +1,16 @@
+# faça um programa que leia um numero inteiro 
+# qualquer e mostre na tela a sua tabuada
+
+# n = int(input('coloque o numero q queira saber a tabuada: '))
+# um = n*1
+# ds = n*2
+# ts = n*3
+# qt = n*4
+# cc = n*5
+# ss = n*6
+# st = n*7
+# ot = n*8
+# nv = n*9
+# dez = n*10
+# print('Tabuada de {}'.format(n))
+# print('{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}'.format(um, ds, ts, qt, cc, ss, st, ot, nv, dez ))

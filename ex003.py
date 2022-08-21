994198079807d2f75ec88ce8d29349b6afe8ff73
LLysame <athos.48yt@gmail.com>
2022-08-21 10:23:09 -0700

    exercicio

--- ex003.py
+++ ex003.py
@@ -4,3 +4,10 @@
 # n2 = int(input('digite outro numero: '))
 # soma = n1*n2
 # print('o resultado de {} vezes {} é igual a {}'.format(n1,n2,soma))
+
+#recriando o codigo de um jeito diferente
+#n1 = int(input('coloque um numero: '))
+#n2 = int(input('coloque outro numero: '))
+#print('agora iremos somar os numero {} e {}'.format(n1,n2))
+#resultado = n1+n2
+#print('o resultado da soma é {}'.format(resultado))

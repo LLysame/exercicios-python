d28d39755d2db1d513c697fe6c2991d68c6f2072
LLysame <athos.48yt@gmail.com>
2022-08-21 16:45:50 -0700

    exercicio

--- ex007.py
+++ ex007.py
@@ -5,3 +5,10 @@
 # n2 = int(input('Segunda nota: '))
 # M = ((n1+n2)/2)
 # print('A nota do aluno é {}'.format(M))
+
+#nota1 = int(input('nota do aluno 1ºbim: '))
+#nota2 = int(input('nota do aluno 2ºbim: '))
+#nota3 = int(input('nota do aluno 3ºbim: '))
+
+#media = (nota1 + nota2 + nota3)/3
+#print('a nota do aluno foi de {}'.format(media))

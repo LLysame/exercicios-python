d80d3a3a03dc562303d684810de931aadfe2083e
LLysame <athos.48yt@gmail.com>
2022-08-21 11:00:38 -0700

    exercicio

--- ex006.py
+++ ex006.py
@@ -7,3 +7,11 @@
 # raiz = n**(1/2)
 # print ('o numero {} tem como {} seu dobro,'.format(n, dobro), end=' ')
 # print('tem como {} seu triplo, tem como {:.3} sua raiz'.format(triplo, raiz))
+
+print('quer saber o dobro ou o triplo ou ate mesmo sua raiz do seu numero ?')
+n = int(input('coloque seu numero: '))
+dobro = n*2
+triplo = n*3
+raiz = n**(1/2)
+print('o numero {} tem como {} seu dobro,'.format(n,dobro), end=' ')
+print('tem como {} seu triplo, tem como {:.3} sua raiz'.format(triplo,raiz))

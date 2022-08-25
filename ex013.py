dd47691f57a66d540b6310438939a8377d14485b
LLysame <athos.48yt@gmail.com>
2022-08-25 09:16:05 -0400

    exercicio

---
+++ ex013.py
@@ -0,0 +1,7 @@
+# fça um algoritmo que leia o salario de um funcionário e
+# mostre seu novo salário, com 15% de aumento.
+
+# print('Qual e o seu salário ?')
+# salario = float(input('R$'))
+# aumento = salario + (salario*15/100)
+# print('seu novo salario é de {}'.format(aumento))

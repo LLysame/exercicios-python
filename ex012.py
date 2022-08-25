d419fbed5cd4052b175a962a264ad31342dddb6a
LLysame <athos.48yt@gmail.com>
2022-08-25 09:04:09 -0400

    exercicio

---
+++ ex012.py
@@ -0,0 +1,8 @@
+# faça um algoritmo que leia seu produto e mostre
+# seu novo preço, com 5% de desconto
+
+# print('produto com 5 de desconto')
+# Produto = float(input('O produto vale R$: '))
+# M = Produto * 5
+# D = Produto - (M / 100)
+# print('O novo valor de R${:.2f} agora é {:.2f} com desconto'.format(Produto, D))

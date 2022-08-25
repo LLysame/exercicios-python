36c664b63f8e00cfc5816609f6eec5ca3e4a61b3
LLysame <athos.48yt@gmail.com>
2022-08-25 08:35:17 -0400

    exercicio

---
+++ ex010.py
@@ -0,0 +1,8 @@
+#crie um programa que leia quanto dinheiro uma pessoa tem na
+#carteira e mostre quantos dólares ela pode comprar.
+#(considere US$1,00 = R$3,27)
+
+#print('Quanto vc tem na Carteira?')
+#real = float(input('R$'))
+#dolar = real / 3.27
+#print('com R${:.2f} você pode comprar US${:.2f}'.format(real,dolar))

7695696d61b589b9624bd8338f83d3f2003ab786
LLysame <athos.48yt@gmail.com>
2022-08-27 10:35:10 -0400

    exercicio

---
+++ ex015.py
@@ -0,0 +1,13 @@
+# Aluguel de carro
+# Escreva um programa que pergunte a quantidade de KM percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado.
+# Calcule o preço a pagar,sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
+
+# print('calculadora para pagar o aluguel do carro')
+# km = float(input('coloque quantos quilometros foi rodado: '))
+# dias = int(input('informe quantos dias o carro foi alugado: '))
+# tdias = dias * 60
+# tkm = km * 0.15
+# print('o total a pagar de dias é de: R${}'.format(tdias))
+# print('o total de quilometros a pagar e de: R${:.2f}'.format(tkm))
+# pagar = tdias + tkm
+# print('total a pagar é de: R${:.2f}'.format(pagar))

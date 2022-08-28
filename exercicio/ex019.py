ba0f578019a6577d25101d35cb4c78b1a8fbceb0
LLysame <athos.48yt@gmail.com>
2022-08-27 23:59:57 -0400

    exercicio

---
+++ exercicio/ex019.py
@@ -0,0 +1,14 @@
+# um professor quer sortear um dos seus 
+# quatros alunos para apagar o quadro. 
+# faça um programa que ajude ele, lendo o 
+# nome deles e escrevendo o nome do 
+# escolhido.
+
+# import random
+# nome1 = str(input('digite o nome do aluno: '))
+# nome2 = str(input('digite o nome do segundo aluno: '))
+# nome3 = str(input('digite o nome do 3 aluno:'))
+# nome4 = str(input('digite o nome do 4 aluno: '))
+# lista = [nome1,nome2,nome3,nome4]
+# escolhido = random.choice(lista)
+# print('O aluno escolhido foi {}'.format(escolhido))

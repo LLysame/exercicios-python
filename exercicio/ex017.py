c02bd4165bf3be71ef5e0ec7985c37c7d45b1c95
LLysame <athos.48yt@gmail.com>
2022-08-27 23:10:15 -0400

    exercicio

---
+++ exercicio/ex017.py
@@ -0,0 +1,10 @@
+# faça um programa que leia o comprimento do cateto oposto 
+# e do cateto adjacente de um triangulo retangulo, calcule 
+# e mostre o comprimento da hipotenusa
+
+# from math import sqrt
+# Ca = float(input('digite o cateto oposto: '))
+# Co = float(input('digite o cateto adjacente: '))
+# hipotenusa = (Ca**2) + (Co**2)
+# raiz = sqrt(hipotenusa)
+# print('A hipotenusa desse triaangulo é: {:.2f}'.format(raiz))

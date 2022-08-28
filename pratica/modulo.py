52a34a337b24e6e3bc0170193c295b9dd2a21243
LLysame <athos.48yt@gmail.com>
2022-08-27 22:16:13 -0400

    Pratica

---
+++ pratica/modulo.py
@@ -0,0 +1,12 @@
+# import math
+# num = int(input('digite um numero'))
+# raiz = math.sqrt(num)
+# print('a raiz de {} é igual a {}'.format(num, math.ceil(raiz)))
+
+# from math import ceil, sqrt, floor
+# num = int(input('digite um numero'))
+# raiz = sqrt(num)
+# print('a raiz de {} é igual a {} maior seria {}'.format(num, floor(raiz), ceil(raiz)))
+
+# import emoji
+# print(emoji.emojize("ola, mundo :thumbs_up:"))

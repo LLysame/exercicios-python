f9c147f18bd5955a3bfc8690a37a12bca8dc3675
LLysame <athos.48yt@gmail.com>
2022-08-27 22:43:52 -0400

    exercicio

---
+++ exercicio/ex016.py
@@ -0,0 +1,8 @@
+# crie um programa que leia um numero real qualquer pelo
+# teclado e mostre na tela a sua porção inteira.
+# ex: digite número:6.127 o número 6127 tem a parte inteira 6.
+
+# import math  
+# num = float(input('digite um numero'))
+# redondo = math.ceil(num)
+# print('o redondo de {} é {}'.format(num, redondo))

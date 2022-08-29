946defbdcd34f5fa1d802b87aa27a0d45c1a6cc5
LLysame <athos.48yt@gmail.com>
2022-08-29 00:15:55 -0400

    exercicio

---
+++ exercicio/ex020.py
@@ -0,0 +1,16 @@
+#o mesmo professor do desafio anterior 
+# quer sortear a ordem de apresentação 
+# de trabalhos dos alunos. faça um 
+# programa que leia o nome dos alunos 
+# e mostre a ordem sorteada
+
+import random
+n = str(input('nome do aluno 1: '))
+n1 = str(input('nome do aluno 2: '))
+n2 = str(input('nome do aluno 3: '))
+n3 = str(input('nome do aluno 4: '))
+
+lista = [n,n1,n2,n3]
+random.shuffle(lista)
+print('Ordem de apresentção')
+print('{}'.format(lista))

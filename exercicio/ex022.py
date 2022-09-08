593a0b20d4b036840dff2f5a4c452a80d4ebb28f
LLysame <athos.48yt@gmail.com>
2022-09-08 09:21:12 -0400

    Exercicio

---
+++ exercicio/ex022.py
@@ -0,0 +1,14 @@
+# Crie um programa que leia o nome completo de uma pessoa e mostre:
+# O nome com todas as letras maiúsculas:
+# o nome com todas minúsculas:
+# Quantas letras ao todo (sem considerar espaços):
+# Quantas letras tem o primeiro nome:
+
+# nome = str(input('Digite seu nome: ')).strip()
+# print('Analisando seu nome...')
+# print('Seu nome em Maiúsculo fica assim:',nome.upper())
+# print('Seu nome em minúsculo fica assim:',nome.lower())
+# print('seu nome tem {} de letras'.format(len(nome) - nome.count(' ')))
+# print('Seu primeiro nome tem {} total de letras'.format(nome.find(' ')))
+# separa = nome.split()
+# print('seu primeiro nome é {} e tem {} total de letras'.format(separa[0], len(separa[0])))

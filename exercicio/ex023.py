8ab980fb45dabeeb6e439401b843f10d661fa1b0
LLysame <athos.48yt@gmail.com>
2022-09-08 09:47:13 -0400

    Exerrcicio

---
+++ exercicio/ex023.py
@@ -0,0 +1,13 @@
+# faça um programa que lleia um número de 0 a 9999 e mostre
+# na tela cada um dos digitos separados
+
+# número = int(input('Digite um numero: '))
+# U = número // 1 % 10
+# M = número // 1000 % 10
+# C = número // 100 % 10
+# D = número // 10 % 10
+# print('Analisando...')
+# print('Milhar: {}'.format(M))
+# print('Centena: {}'.format(C))
+# print('Dezenas: {}'.format(D))
+# print('Unidade: {}'.format(U))
---
+++ exercicio/ex024.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia o nome de uma cidade e diga se ela 
+# começa ou não com o nome "santos"
+
+# nome = str(input('Em qual cidade vc nasceu: ')).strip()
+# print(nome[:5].upper == 'SANTOS')

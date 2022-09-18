8ede1031fa4cda0980161317ee6f1879456d8511
LLysame <athos.48yt@gmail.com>
2022-09-18 14:54:44 -0400

    Exercicio

---
+++ exercicio/ex033.py
@@ -0,0 +1,23 @@
+#Faça um programa que leia três números 
+# e mostre qual é o maior e qual e o menor
+from calendar import c
+from re import A
+
+
+# a = int(input('Digite um numero: '))
+# b = int(input('Digite o proximo numero: '))
+# c = int(input('Digite o ultimo numero: '))
+    #  //Verificando qual e o menor valor
+# menor = a
+# if b<a and b<c:
+#     menor = b
+# if c<a and c<b:
+#     menor = c
+    #  //Verificar qual e o maior numero
+# maior = a
+# if b>a and b>c:
+#     maior = b
+# if c>a and c>b:
+#     maior = c
+# print('o menor numero é {}'.format(menor))
+# print('o maior numero e {}'.format(maior))

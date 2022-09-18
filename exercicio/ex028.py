b72ba11966b406a85d94188bec157b9634a1067e
LLysame <athos.48yt@gmail.com>
2022-09-17 21:50:13 -0400

    Exercicio

---
+++ exercicio/ex028.py
@@ -0,0 +1,22 @@
+# escreva um programa que faça o computador
+# "pensar" em um numero inteiro entre 0 a 5 e peça para o
+# usuario tentar descobrir qual foi o numero escolhido pelo
+# computador.
+# O programa deverá escrever na tela se o usuário
+#  venceu ou perdeu.
+
+# from random import randint
+# print(60*'=')
+# print('Pensamentos pensantes !!!')
+# print(60*'=')
+# computador = randint(0, 5)  #escolhe um número de 0 a 5 aleatoriamente.
+# print('--------Regras--------')
+# print('números de 0 a 5;')
+# print('apenas 1 número;')
+# print('para jogar de novo e necessario reiniciar o jogo!')
+# jogador = int(input('Qual número o computador está pensando?: '))  #jogador fala um número que pensa q o computador escolheu
+# if computador == jogador:  #"se" o numero que computador escolheu for igual ao numero do jogador
+#     print('você acertou!!!! :)') #então aparece isso
+# else:  #senão for
+#     print('você errou! :(')  #então aparece isso
+# print('==========================FIM===============================') #fim do programa

2482cf06778dbf931b48a26585e51c9856a25701
LLysame <athos.48yt@gmail.com>
2022-09-17 22:58:09 -0400

    Exercicio

---
+++ exercicio/ex030.py
@@ -0,0 +1,8 @@
+#Crie um programa que leia um número inteiro 
+# e mostre na tela se ele é PAR ou ÍMPAR.
+
+# print('vou advinhar se o numero e impar ou par')
+# x = int(input('digite um número: '))
+# resultado = x%2
+# print('número e par'if resultado==0 else 'número e ímpar')
+# print('====================fim=====================')

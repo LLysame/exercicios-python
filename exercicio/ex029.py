6ed52a8a1a3ee67e2988bb939af8c0965bcca654
LLysame <athos.48yt@gmail.com>
2022-09-17 22:38:28 -0400

    Exercico

---
+++ exercicio/ex029.py
@@ -0,0 +1,13 @@
+#Escreva um programa que leia a velocidade de um carro. Se ele
+#ultraprassar 80km/h, mostre uma mensagem dizendo que ele foi
+#multado. A multa vai custar R$7,00 por cada km acima do
+# limite.
+
+# print(60*'=')
+# v_carro = int(input('Qual foi a velocidade do carro?: '))
+# multa = 7*(v_carro-80)
+# if v_carro <= 80:
+#     print('Figue Tranquilo, não vai pagar nada, pode ir com segurança!')
+# else:
+#     print('PARADO AI !!, vc deve pagar uma multa de R${:.2f}'.format(multa))
+# print('Volte com segurança!')

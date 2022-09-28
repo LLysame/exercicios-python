de0a6c9ff01b11f18b6bf4b714110b8218cecbde
LLysame <athos.48yt@gmail.com>
2022-09-27 21:32:06 -0400

    Exercicio

--- exercicio/ex035.py
+++ exercicio/ex035.py
@@ -2,10 +2,10 @@
 # retas e diga ao usuario se elas podem ou não formar 
 # um triangulo.
 
-raio1 = float(input('Primeiro numero: '))
-raio2 = float(input('Segundo numero: '))
-raio3 = float(input('Terceiro numero: '))
-if raio1<raio2+raio3 and raio2<raio1+raio3 and raio3<raio1+raio2:
-    print('Então pode fazer um tringulo!!!!!')
-else:
-    print('Não pode fazer um triangulo!!!!')
+# raio1 = float(input('Primeiro numero: '))
+# raio2 = float(input('Segundo numero: '))
+# raio3 = float(input('Terceiro numero: '))
+# if raio1<raio2+raio3 and raio2<raio1+raio3 and raio3<raio1+raio2:
+#     print('Então pode fazer um tringulo!!!!!')
+# else:
+#     print('Não pode fazer um triangulo!!!!')

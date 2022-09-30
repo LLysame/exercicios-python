9a8251eb29d01bf6814196a906ee7ea100a36b9f
LLysame <athos.48yt@gmail.com>
2022-09-30 13:53:13 -0400

    Exercicio

---
+++ exercicio/ex040.py
@@ -0,0 +1,17 @@
+# Crie um prigrama que leia duas notas de um aluno e
+# calcule sua média, mostrando uma mensagem no final, de
+# acordo com a média atingida:
+# Média abaixo de 5,0: Reprovado
+# Média entre 5,0 e 6,9: Reculperação
+# Média 7,0 ou superior: Aprovado
+
+# n1 = float(input('digite a primeira nota: '))
+# n2 = float(input('digite a segunda nota: '))
+# r = (n1+n2)/2
+# print('suas notas são {} e {}, tem um total de {}'.format(n1,n2,r))
+# if r < 5.0:
+#     print('aluno foi REPROVADO!')
+# elif r >= 5.0 and r <= 6.9:
+#     print('você está em RECUPERAÇÃO!')
+# else:
+#     print('você foi APROVADO!!')

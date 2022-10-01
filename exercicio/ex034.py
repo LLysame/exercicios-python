bdf39495bba0710029698c356aa18f5b521c6b69
LLysame <athos.48yt@gmail.com>
2022-09-30 22:44:11 -0400

    Exercicio

--- exercicio/ex034.py
+++ exercicio/ex034.py
@@ -3,13 +3,13 @@
 #Para salario superiores a R$1.250,00, calcule um aumento de
 #  10%. Para os inferiores ou iguais o aumento e de 15%.
 
-print('='*60)
-Qual_valor = int(input('qual o seu ?: '))
-print('-'*60)
-if Qual_valor <= 1250:
-    novo_salario = Qual_valor + (Qual_valor * 15 / 100)
-    print('seu novo salario e de {} !'.format(novo_salario))
-else:
-    novo_salario = Qual_valor - (Qual_valor * 10 / 100)
-    print('seu novo salario e de {}'.format(novo_salario))
-print('='*60)
+# print('='*60)
+# Qual_valor = int(input('qual o seu ?: '))
+# print('-'*60)
+# if Qual_valor >= 1250:
+#     novo_salario = Qual_valor + (Qual_valor * 15 / 100)
+#     print('seu novo salario e de {} !'.format(novo_salario))
+# else:
+#     novo_salario = Qual_valor + (Qual_valor * 10 / 100)
+#     print('seu novo salario e de {}'.format(novo_salario))
+# print('='*60)

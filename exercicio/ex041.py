e2ea4aa1b3a0ff40541cf301fad865b9e399f90d
LLysame <athos.48yt@gmail.com>
2022-10-04 09:11:22 -0400

    Exercicio

---
+++ exercicio/ex041.py
@@ -0,0 +1,24 @@
+# A configuração Nacional precisa de um programa 
+# que leia o ano de nascimento de um atleta e 
+# mostre sua categoria, de acordo com a idade:
+# -até 9 anos: MIRIN
+# -até 14 anos:INFANTIL
+# -até 19 anos: JUNIOR
+# -até 25 anos: SÊNIOR
+# -Acima: MASTER
+
+# from datetime import date
+# dat = date.today().year
+# nasc = int(input('Qual ano você nasceu?: '))
+# id = dat-nasc
+# if id <= 9:
+#     print('Sua categoria é: MIRIN')
+# elif id<=14:
+#     print('Sua categoria é: INFANTIL')
+# elif id<=19:
+#    print('Sua categoria é: JUNIOR')
+# lif id<=25:
+#     print('Sua categoria é: SÊNIOR')
+# lse:
+#    print('Sua categoria é: MASTER')
+# rint('Sua idade é {} anos.'.format(id))
